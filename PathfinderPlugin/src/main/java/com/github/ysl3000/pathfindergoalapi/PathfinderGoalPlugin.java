6d4ba38a37ccffacf1441dd61fe4ea4ea353d891
ysl3000 <yannicklamprecht@live.de>
2017-08-17 15:04:08 +0200

    added travis.yml

---
+++ PathfinderPlugin/src/main/java/com/github/ysl3000/pathfindergoalapi/PathfinderGoalPlugin.java
@@ -0,0 +1,18 @@
+package com.github.ysl3000.pathfindergoalapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PathfinderGoalPlugin extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+    }
+}
