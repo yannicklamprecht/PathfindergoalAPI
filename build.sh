977e18ca88f352d41af929539819ab8742a867df
ysl3000 <yannicklamprecht@live.de>
2019-12-12 14:31:09 +0100

    task: Adapted docs generation in build script

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ export MAVEN_OPTS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1"
 
 mvn clean install -P Impl -T 1C -DdependencyLocationsEnabled=false
 
+rm -rf docs/
 (
 cd PathfinderApi/ || exit
 mvn dokka:javadoc
